0a13702d9a50daeb96ce947eaa391c6719ac894a
drewphillips <drew@superheroprinting.com>
2018-07-12 12:40:38 -0700

    some code

---
+++ letters.js
@@ -0,0 +1,11 @@
+function letter(a)
+
+this.trueLetter=a
+
+this.letterGuessed=false;
+
+this.display=function(
+    check this.letterGuessed
+    return this.letter
+    
+)
---
+++ word.js
@@ -0,0 +1,17 @@
+var Letter = require (./bootcamp/homework/letter)
+
+
+function Word(word){
+    this.letters = [];
+
+
+
+}
+
+var strArray = word.split();
+for (var i = 0; i< strArray.lenmgth; i++)
+{
+    this.letters.push( new Letter(stringArray[i]) );
+
+}
+
